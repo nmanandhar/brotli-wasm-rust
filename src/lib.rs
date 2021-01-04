5c8ca0904ea7f8b0ff78daca380fcaa3375e97f9
nmanandhar <nmanandhar777@gmail.com>
2021-01-04 10:28:31 +0545

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+extern crate brotli;
+
+use std::io::Write;
+
+#[wasm_bindgen]
+pub fn compress(input: &[u8], buffer_size: usize, quality: u32, lg_window_size: u32) -> Vec<u8> {
+    let mut output = Vec::new();
+    {
+        let mut writer = brotli::CompressorWriter::new(&mut output, buffer_size, quality, lg_window_size);
+        writer.write(input).unwrap();
+    }
+    return output;
+}
+
+#[wasm_bindgen]
+pub fn decompress(input: &[u8], buffer_size: usize) -> Vec<u8> {
+    let mut output = Vec::new();
+    {
+        let mut writer = brotli::DecompressorWriter::new(&mut output, buffer_size);
+        writer.write(&input).unwrap();
+    }
+    return output;
+}
